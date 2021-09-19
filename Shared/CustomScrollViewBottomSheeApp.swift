3f49df7589a933e886ba860a42d847defe78610d
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-09-19 21:35:32 +0200

    Initial Commit

---
+++ Shared/CustomScrollViewBottomSheeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomScrollViewBottomSheeApp.swift
+//  Shared
+//
+//  Created by Michele Manniello on 19/09/21.
+//
+
+import SwiftUI
+
+@main
+struct CustomScrollViewBottomSheeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
